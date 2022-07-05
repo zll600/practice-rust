a9ce4e65d6f4a898dcdc381413a79d031ac50e18
zll600 <3400692417@qq.com>
2022-07-06 07:49:11 +0800

    delete guessing_game/target directory

--- guessing_game/target/debug/deps/guessing_game-3f658c19dd973748.d
+++
@@ -1,5 +0,0 @@
-/home/andy/Code/rust/practice-rust/guessing_game/target/debug/deps/guessing_game-3f658c19dd973748: src/main.rs
-
-/home/andy/Code/rust/practice-rust/guessing_game/target/debug/deps/guessing_game-3f658c19dd973748.d: src/main.rs
-
-src/main.rs:
--- guessing_game/target/debug/guessing_game.d
+++
@@ -1,1 +0,0 @@
-/home/andy/Code/rust/practice-rust/guessing_game/target/debug/guessing_game: /home/andy/Code/rust/practice-rust/guessing_game/src/main.rs
