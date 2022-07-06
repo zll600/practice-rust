88a4bb767383f5ae85bff20baaaedffd97c88d6e
zll600 <3400692417@qq.com>
2022-07-06 08:23:34 +0800

    Add infitine loop read user input

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -7,22 +7,31 @@ fn main() {
     
     let secret_number = rand::thread_rng().gen_range(1..=100);
 
-    println!("The secret_number is {secret_number}");
-    println!("Please input your guess.");
+    // println!("The secret_number is {secret_number}");
 
-    let mut guess = String::new();
+    loop {
+        println!("Please input your guess.");
 
-    io::stdin()
-        .read_line(&mut guess)
-        .expect("Failed to read line");
+        let mut guess = String::new();
 
-    let guess: u32 = guess.trim().parse().expect("Please type a number");
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read line");
 
-    println!("You guessed: {guess}");
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
 
-    match guess.cmp(&secret_number) {
-        Ordering::Less => println!("Too Small!"),
-        Ordering::Greater => println!("Too big!"),
-        Ordering::Equal => println!("You win!"),
+        println!("You guessed: {guess}");
+
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too Small!"),
+            Ordering::Greater => println!("Too big!"),
+            Ordering::Equal => {
+                println!("You win!");
+                break;
+            }
+        }
     }
 }
